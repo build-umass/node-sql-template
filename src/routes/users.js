1b965d7573d0baa99942b480d9c684b03b5855f9
Andrew Zhang <54557530+ayz-lex@users.noreply.github.com>
2020-03-07 23:19:29 -0500

    Implement add users route to DB (#2)
    * Implement POST route for adding new users to db
    
    * Add example curl request
    
    Co-authored-by: Vedant Roy <vroy101@gmail.com>

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,5 +1,6 @@
 const knex = require('../database/database')
-const router = require('express').Router()
+const express = require('express')
+const router = express.Router()
 // Probably not the best way to do this in a real app.
 // (Unclear what the best way is)
 const USERS_TABLE = "users"
@@ -16,12 +17,32 @@ router.get('/:user', async (req, res) => {
 
 // POST [base]/[a username] adds a new
 // user with the given information to the "users" table
-// if and only if that user does not exist
-router.post('/:user', async (req, res) => {
-    // TODO: Get the username
-    // Find this user in the database
-    // Update the row in the database with the new user information
-    // This information is stored in the post request body as a JSON string
+// or updates an existing user
+// Example client request:
+//curl -X POST -d '{"oranges":12}' -H "Content-Type: application/json"  http://localhost:8080/users/bill
+router.post('/:user', express.json(), async (req, res) => {
+    if (req.body === undefined || req.body.oranges === undefined) {
+        // TODO:
+        // throw an exception (for middleware to handle later)
+        // OR set the status code to 400 (Bad Request)
+        // and use res.json/res.send an appropriate error message
+    }
+    const oranges = req.body.oranges
+    const user = req.params.user
+
+    // Possible TODO: This requires 2 SQL queries
+    // 1. to determine if the user exists
+    // 2. to update the relevant information
+    // A solution involves using "upsert" (INSERT ... ON CONFLICT)
+    const rows = await knex(USERS_TABLE).where({username: user})
+    if (rows.length === 0) {
+        await knex(USERS_TABLE).insert({username: user, oranges: oranges})
+    } else {
+        await knex(USERS_TABLE).where({username: user}).update({oranges: oranges})
+    }
+    // TODO: figure out a return convention.
+    // Should we return a JSON response with a "status" field?
+    res.json({status: 200})
 })
 
 module.exports = router
